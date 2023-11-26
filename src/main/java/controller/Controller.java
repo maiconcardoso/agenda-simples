f0c96fb6db397cb9f4ba5d3e4902d4da5a13a9c7
Maicon Cardoso <maiconscardoso@hotmail.com>
2023-11-26 18:13:23 -0300

    Configurando a camada controller no servlet

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,28 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.Dao;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns= {"/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	Dao dao = new Dao();
 
-    /**
-     * Default constructor. 
-     */
-    public Controller() {
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.print("Servlet funcionando!");
+	public Controller() {
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		if (action.equals("/main")) {
+			contatos(request, response);
+		}
+
+	}
+
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+
+	}
 
 }
